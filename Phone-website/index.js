d86a704145dd5e86cb7f469ad3721273c040fefa
Emman091 <emmanuelkenneth063@mail.com>
2026-05-27 21:36:00 +0100

    Add TaxFlo dashboard

--- Phone-website/index.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.wishlist').forEach(heart => {
-    heart.addEventListener('click', () => {
-        heart.style.color = heart.style.color === 'red' ? '#e6a06c' : 'red'
-    })
-})
